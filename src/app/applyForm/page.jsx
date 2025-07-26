3ae0758dece822e14aa085d78994e7d386ffca6d
Umesh Joshi <uj19343@gmail.com>
2025-07-26 18:16:10 +0545

    added new feature in the user applyonPost

--- src/app/applyForm/page.jsx
+++ src/app/applyForm/page.jsx
@@ -94,7 +94,7 @@ const FormPost = () => {
         toast.error(data.message);
       }
     } catch (error) {
-      toast.error(data.message);
+      toast.error(error, "hey");
     }
   };
   const [resumeName, setResumeName] = useState("");
@@ -168,6 +168,38 @@ const FormPost = () => {
                     className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
                   />
                 </div>
+                <div>
+                  <label
+                    htmlFor="email"
+                    className=" text-sm font-medium text-gray-700 mb-1 flex items-center"
+                  >
+                    <UserIcon className="mr-2" />
+                    email*
+                  </label>
+                  <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    required
+                    className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="number"
+                    className=" text-sm font-medium text-gray-700 mb-1 flex items-center"
+                  >
+                    <UserIcon className="mr-2" />
+                    number*
+                  </label>
+                  <input
+                    type="number"
+                    id="number"
+                    name="number"
+                    required
+                    className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                  />
+                </div>
               </div>
             </div>
 
@@ -192,6 +224,24 @@ const FormPost = () => {
                   min="0"
                   required
                   className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                />
+              </div>
+              <div className="max-w-xs">
+                <label
+                  htmlFor="linkdenProfile"
+                  className="text-sm font-medium text-gray-700 mb-1 flex items-center"
+                >
+                  <BriefcaseIcon className="mr-2" />
+                  LinkedIn profile *
+                </label>
+                <input
+                  type="url"
+                  id="linkdenProfile"
+                  name="linkdenProfile" // Matches backend
+                  pattern="https?://(www\.)?linkedin\.com/.*"
+                  required
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                  placeholder="https://linkedin.com/in/username"
                 />
               </div>
             </div>
